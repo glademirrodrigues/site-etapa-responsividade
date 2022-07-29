b3395e243096c8d213fb084acd02264e435d0b4e
Glademir Rodrigues <rodriguesglademir@gmail.com>
2022-07-28 23:49:57 -0300

    atividade er3

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function mostrarpopup(){
+    window.alert("Fala mestre! (a). Seja bem vindo (a)")
+}
+
+let email = document.getElementById("campo-email");
+
+function enviaremail(){
+    let emaildigitado = email.value;
+    console.log(emaildigitado)
+}
